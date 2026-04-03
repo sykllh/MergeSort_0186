dab4b75f67785420e3d5b415112ea6a650dc80ed
sykllh <sykllhidmi@gmail.com>
2026-04-03 14:34:47 +0700

    buat fungsi merge sort

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -33,3 +33,13 @@ void input()
         cin >> arr[i];
     }
 }
+
+// create function mergeSort
+void mergeSort(int low, int high)
+{
+    if (low >= high) // step 1
+    {
+        return; // step 1.a
+    }
+
+    int mid = (low + high) / 2; // step 2
