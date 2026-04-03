54512df3000d0a11e0f15b6f68fd2b7a7f9f2d33
sykllh <sykllhidmi@gmail.com>
2026-04-03 14:36:43 +0700

    fungsi rekursif memanggil step sendiri

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -43,3 +43,8 @@ void mergeSort(int low, int high)
     }
 
     int mid = (low + high) / 2; // step 2
+
+     // step 3
+    // fungsi rekursif memanggil step sendiri
+    mergeSort(low, mid);       // step 3.a
+    mergeSort(mid + 1, high);  // step 3.b
