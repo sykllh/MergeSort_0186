aea9763e6d7f614fd5f42938ab420111de177228
sykllh <sykllhidmi@gmail.com>
2026-04-03 14:28:22 +0700

    membuat fungsi panjang array

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -5,3 +5,17 @@ using namespace std;
 int arr[20], B[20];
 // n is array input size
 int n;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
